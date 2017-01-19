01a45296f30b66d5e83f2ab1f22d2e9a75e83c4f
陈钦辉 <hzchenqinhui@corp.netease.com>
2017-01-19 15:56:51 +0800

    @dev main 主动发生消息

--- main-process/communication/async-msg.js
+++ main-process/communication/async-msg.js
@@ -1,5 +1,8 @@
 const ipc = require('electron').ipcMain
 
 ipc.on('asynchronous-message', function (event, arg) {
+  // main progress 主动向某个render progress 发生消息
+  // mainWindow = new BrowserWindow({});
+  // mainWindow.webContents.send('global-shortcut', 0);
   event.sender.send('asynchronous-reply', 'pong')
 })
